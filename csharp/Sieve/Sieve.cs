fc4eb1ac5b5f56e3aed3354af3af4e5345024555
Voidraizer <Voidraizer@users.noreply.github.com>
2025-09-28 17:08:29 -0400

    Initial Implementation

--- csharp/Sieve/Sieve.cs
+++ csharp/Sieve/Sieve.cs
@@ -1,14 +1,46 @@
-﻿namespace Sieve;
+﻿using System.Diagnostics;
+
+namespace Sieve;
 
 public interface ISieve
 {
-    long NthPrime(long n);
+    long NthPrime( long n );
 }
 
 public class SieveImplementation : ISieve
 {
-    public long NthPrime(long n)
+    public long NthPrime( long n )
     {
-        throw new NotImplementedException("unimplemented");
+        // Base slow method
+        HashSet<int> primes = new HashSet<int>();
+
+        bool found = false;
+        int counter = 2;
+
+        while( !found )
+        {
+            bool isPrime = primes.All( i => counter % i != 0 );
+
+            if( isPrime )
+            {
+                primes.Add( counter );
+            }
+
+            if( primes.Count - 1 == n )
+            {
+                found = true;
+                List<string> suffixes = new List<string> { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+                int val = (int)n + 1;
+                val = int.Parse( val.ToString().Substring( val.ToString().Length - 1 ) );
+
+                Debug.Print( $"Found the {n + 1}{suffixes[val]} prime: {counter}" );
+            }
+            else
+            {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 }
