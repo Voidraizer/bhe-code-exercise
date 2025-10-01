74ac866d91337826c581ebf0d33165fa9b747a4b
Voidraizer <Voidraizer@users.noreply.github.com>
2025-10-01 14:21:15 -0400

    Updated unit tests to include even bigger ones

--- csharp/Sieve.Tests/UnitTest1.cs
+++ csharp/Sieve.Tests/UnitTest1.cs
@@ -24,9 +24,12 @@ namespace Sieve.Tests
             Assert.AreEqual( 1299709, sieve.NthPrime( 99999 ) );
             Assert.AreEqual( 15485867, sieve.NthPrime( 1000000 ) );
             Assert.AreEqual( 179424691, sieve.NthPrime( 10000000 ) );
-            Assert.AreEqual( 1189457257, sieve.NthPrime( 59950298 ) ); // max version 1 can handle before int overflow
-            Assert.AreEqual( 2038074751, sieve.NthPrime( 100000000 ) ); // not required, just a fun challenge
-            Assert.AreEqual( 2902958803, sieve.NthPrime( 140000000 ) ); // bigger challenge
+            Assert.AreEqual( 1189457257, sieve.NthPrime( 59950298 ) ); // max version 1 can handle before overflow
+            Assert.AreEqual( 2038074751, sieve.NthPrime( 100000000 ) ); // not required, just a fun challenge - ~8 seconds on my desktop
+            //Assert.AreEqual( 4222234763, sieve.NthPrime( 200000000 ) ); // bigger fun challenge - ~19 seconds on my desktop
+            //Assert.AreEqual( 11037271769, sieve.NthPrime( 500000000 ) ); // bigger challenge - ~2 mins on my desktop
+            //Assert.AreEqual( 22801763513, sieve.NthPrime( 1000000000 ) ); // bigger challenge - ~3.7 mins on my desktop. P.S. Wolfram Alpha stops calculating primes after the 1 billionth prime
+            //Assert.AreEqual( 47055833479, sieve.NthPrime( 2000000000 ) ); // bigger challenge - ~10 mins on my desktop
         }
     }
 }
